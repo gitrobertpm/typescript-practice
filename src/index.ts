d633203f31f83dd3430d75dda34d2b3cd110b5bb
robertpm <grobertpm@gmail.com>
2021-09-09 02:41:59 -0700

    Added custom error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@
  import express from "express";
  import cors from "cors";
  import helmet from "helmet";
+
  import { itemsRouter } from './items/items.router';
+ import { errorHandler } from "./middleware/error.middleware";
+ import { notFoundHandler } from "./middleware/not-found.middleware";
  
  dotenv.config();
 
@@ -15,7 +18,7 @@
  * App Variables
  */
 
- if (!process.env.PORT) {
+if (!process.env.PORT) {
   process.exit(1);
 }
 
@@ -28,16 +31,19 @@ const app = express();
  *  App Configuration
  */
 
- app.use(helmet());
- app.use(cors());
- app.use(express.json());
- app.use('/api/menu/items', itemsRouter);
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use('/api/menu/items', itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 
 /**
  * Server Activation
  */
 
- app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
